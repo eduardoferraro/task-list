189a0f23ec2348134fcb16037b891bbd537b7cea
Eduardo <eduard.ferraro@gmail.com>
2022-07-30 20:59:59 -0300

    mock task e interface

---
+++ src/app/Tasks.ts
@@ -0,0 +1,6 @@
+export interface tasks {
+    id: number;
+    text: string;
+    day: string;
+    reminder:boolean;
+}
---
+++ src/app/base-tasks.ts
@@ -0,0 +1,33 @@
+
+import { tasks } from "./tasks"
+
+export const TASK:tasks[] = [
+{
+    id: 1,
+    text: 'Terminar modulo Angular',
+    day: 'Un dia de estos',
+    reminder:true
+},
+{
+    id: 2,
+    text: 'Terminar modulo Base de datos',
+    day: 'Otro dia de estos',
+    reminder:true
+},
+{
+    id: 3,
+    text: 'Terminar modulo Inicial',
+    day: 'Mañana',
+    reminder:false
+},
+{
+    id: 4,
+    text: 'Terminar todo el curso',
+    day: 'A fin de año',
+    reminder:false
+}
+
+
+
+
+]
